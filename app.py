f6487f6286613cab141e574c109c32cf60e95813
Tashinga Musanhu <44523844+Tashsub@users.noreply.github.com>
2020-08-18 00:05:53 +0200

    front end implemented

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 #written by: Tashinga 
 
-
 from tkinter import *
-
 
 window=Tk()
 
@@ -11,7 +9,6 @@ a1 = Label(window, text = "Title")
 a1.grid(row=0, column=0)
 a2=Entry(window)
 a2.grid(row=0, column=1)
-
 
 #year field
 a3 =  Label(window, text = "Year")
@@ -29,8 +26,46 @@ a6.grid(row=0, column=3)
 a7 = Label(window, text = "ISBN")
 a7.grid(row=1, column=2)
 a8 = Entry(window)
-a8.grid(row=1, column=3 )
+a8.grid(row=1, column=3)
 
+#attachments
+a9=Label(window, text="Attachments")
+a9.grid(row=0, column=4)
+a10=Button(window, text="Upload", width=10)
+a10.grid(row=1,column=4)
 
+#list box to display content
+a11 = Listbox(window, height=20, width=50)
+a11.grid(row=2,column=0,rowspan=10,columnspan=2)
+
+#scrollbar for listbox
+scroll = Scrollbar(window)
+scroll.grid(row=2, column=2, rowspan=10)
+a11.configure(yscrollcommand=scroll.set)
+scroll.configure(command=a11.yview)
+
+#View all
+viewall=Button(window, text="View All", width = 30, height = 2)
+viewall.grid(row=2, column=3, columnspan=10)
+
+#Search Entry
+search=Button(window, text="Search Entry", width = 30, height = 2)
+search.grid(row=3, column=3, columnspan=10) 
+
+#Add entry
+add=Button(window, text="Add Entry", width = 30, height = 2)
+add.grid(row=4, column=3, columnspan=10) 
+
+#update
+update=Button(window, text="Update Entry", width = 30, height = 2)
+update.grid(row=5, column=3, columnspan=10) 
+
+#Delete
+delete=Button(window, text="Delete Entry", width = 30, height = 2)
+delete.grid(row=6, column=3, columnspan=10) 
+
+#close
+close=Button(window, text="Close", width = 30, height = 2)
+close.grid(row=7, column=3, columnspan=10) 
 
 window.mainloop()
